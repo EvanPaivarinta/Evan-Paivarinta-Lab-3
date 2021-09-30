2b0c96edef954dc07578bad6fa6f07dcfad8a78f
EvanPaivarinta <89535702+EvanPaivarinta@users.noreply.github.com>
2021-09-30 00:38:37 -0500

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -18,23 +18,17 @@ int main()
 	string str;
 	string inputFileName = "C:/Users/Engineering/Source/repos/Evan Paivarinta Lab 3/inMeanStd.dat";
 	string outputFileName = "outMeanStd.dat";
-	int data1, data2, data3, data4;
+	int value1, value2, value3, value4;
 	ifstream inFile;
 	ofstream outFile;
 	inFile.open("inMeanStd.dat");
-	if (inFile.is_open())
-	{
-		while (inFile >> data1, data2, data3, data4);
-	}
-	else {
-		cout << "ERROR" << endl;
-	}
 	outFile.open("outMeanStd.dat");
+	inFile >> value1 >> value2 >> value3 >> value4;
+	outFile << value1 << value2 << value3 << value4;
 	cout << "This will calculate the mean and standard deviation of four user values" << endl;
 	float A1, A2, A3, A4; //assigning values 
-	float mean;
+	float mean; 
 	float standardDeviation;
-	float sd1, sd2, sd3, sd4, sd5, sd6, sd7, sd8;
 	cout << "Please enter your first number: ";
 	cin >> A1;
 	cout << "Please enter your second number ";
@@ -43,17 +37,9 @@ int main()
 	cin >> A3;
 	cout << "Please enter your fourth number ";
 	cin >> A4;
-	mean = ((A1 + A2 + A3 + A4 + data1 + data2 + data3 + data4) / 8);
+	mean = ((A1 + A2 + A3 + A4 + value1 + value2 + value3 + value4) / 8);
 	// mean formula
-	sd1 = ((A1 - mean) *= (A1 - mean));
-	sd2 = ((A2 - mean) *= (A2 - mean));
-	sd3 = ((A3 - mean) *= (A3 - mean));
-	sd4 = ((A4 - mean) *= (A4 - mean));
-	sd5 = ((data1 - mean) *= (data1 - mean));
-	sd6 = ((data2 - mean) *= (data2 - mean));
-	sd7 = ((data3 - mean) *= (data3 - mean));
-	sd8 = ((data4 - mean) *= (data4 - mean));
-	standardDeviation = ((sd1 + sd2 + sd3 + sd4 + sd5 + sd6 + sd7 + sd8) / 8);
+	standardDeviation = (((A1 - mean) * (A1 - mean)) + ((A2 - mean) * (A2 - mean)) + ((A3 - mean) * (A3 - mean)) + ((A4 - mean) * (A4 - mean)) + ((value1 - mean) * (value1 - mean)) + ((value2 - mean) * (value2 - mean)) + ((value3 - mean) * (value3 - mean)) + ((value4 - mean) * (value4 - mean)) / 8);
 	// standard deviation equation
 	cout << "\nYour mean is: " << mean << endl;
 	cout << "\nYour standard deviation is: " << standardDeviation << endl;
